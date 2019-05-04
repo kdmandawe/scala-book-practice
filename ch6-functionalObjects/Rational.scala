1ad2b4bc5e324d25ec412407e3cf074ae163ac06
kmandawe <kdmandawe06@yahoo.com.ph>
2019-05-04 13:39:34 +0800

    Operators in scala

--- ch6-functionalObjects/Rational.scala
+++ ch6-functionalObjects/Rational.scala
@@ -9,19 +9,22 @@ class Rational(n: Int, d: Int) {
 
   def this(n: Int) = this(n, 1) // auxillary constructor
 
-  override def toString: String = numer + "/" + denom
-
-  def add(that: Rational): Rational =
+  def + (that: Rational): Rational =
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
+
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
 
   def lessThan(that: Rational) =
     this.numer * that.denom < that.numer * this.denom
 
   def max(that: Rational) =
     if (this.lessThan(that)) that else this
+
+  override def toString: String = numer + "/" + denom
 
   private def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
