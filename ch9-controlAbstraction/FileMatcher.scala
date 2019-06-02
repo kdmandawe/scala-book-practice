8bc8ccbbc5e071fae6632510f5150c55d9c84fd7
kmandawe <kdmandawe06@yahoo.com.ph>
2019-06-02 17:44:56 +0800

    HOF

--- ch9-controlAbstraction/FileMatcher.scala
+++ ch9-controlAbstraction/FileMatcher.scala
@@ -1,15 +1,30 @@
 object FileMatcher {
   private def filesHere = (new java.io.File(".")).listFiles
 
+//  def filesEnding(query: String) =
+//    for (file <- filesHere; if file.getName.endsWith(query))
+//      yield file
+
   def filesEnding(query: String) =
-    for (file <- filesHere; if file.getName.endsWith(query))
-      yield file
+    filesMatching(query, _.endsWith(query))
+
+//  def filesContaining(query: String) =
+//    for (file <- filesHere; if file.getName.contains(query))
+//      yield file
 
   def filesContaining(query: String) =
-    for (file <- filesHere; if file.getName.contains(query))
-      yield file
+    filesMatching(query, _.contains(query))
+
+//  def filesRegex(query: String) =
+//    for (file <- filesHere; if file.getName.matches(query))
+//      yield file
 
   def filesRegex(query: String) =
-    for (file <- filesHere; if file.getName.matches(query))
+    filesMatching(query, _.matches(query))
+
+  def filesMatching(matcher: (String) => Boolean) = {
+    for (file <- filesHere; if matcher(file.getName))
       yield file
+  }
+
 }
