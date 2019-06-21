b5edcf37b46f1971bc605f6490d479c7f68470a5
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2019-06-21 10:54:08 +0800

    Ordered Trait

---
+++ ch12-traits/Rational.scala
@@ -0,0 +1,55 @@
+class Rational(n: Int, d: Int) extends Ordered[Rational] {
+
+  require(d != 0, "zero not allowed as denominator")
+
+  private val g = gcd(n.abs, d.abs)
+
+  val numer: Int = n / g
+  val denom: Int = d / g
+
+  def this(n: Int) = this(n, 1) // auxillary constructor
+
+  def + (that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  def + (i: Int): Rational =
+    new Rational(numer + i * denom, denom)
+
+  def - (that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+
+  def - (i: Int): Rational =
+    new Rational(numer - i * denom, denom)
+
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+
+  def * (i: Int): Rational =
+    new Rational(numer * i, denom)
+
+  def / (that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+
+  def / (i: Int): Rational =
+    new Rational(numer, denom * i)
+
+  def lessThan(that: Rational) =
+    this.numer * that.denom < that.numer * this.denom
+
+  def max(that: Rational) =
+    if (this.lessThan(that)) that else this
+
+  override def toString: String = numer + "/" + denom
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+
+  override def compare(that: Rational): Int =
+    (this.numer * that.denom) - (that.numer * this.denom)
+}
