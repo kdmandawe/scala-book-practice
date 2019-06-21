44c02aa22b391f3f0add00960337e205bc9c27d9
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2019-06-21 10:18:58 +0800

    Enrichment Trait

--- ch12-traits/Rectangle.scala
+++ ch12-traits/Rectangle.scala
@@ -1,19 +1,32 @@
 class Point(val x: Int, val y: Int)
 
-class Rectangle(val topLeft: Point, val bottomRight: Point) {
-  def left = topLeft.x
-  def right = bottomRight.x
-  def width = right - left
+class Rectangle(val topLeft: Point, val bottomRight: Point) extends Rectanglular {
   // and many more geometric methods...
 }
 
-abstract class Component {
+//abstract class Component {
+//  def topLeft: Point
+//  def bottomRight: Point
+//
+//  def left = topLeft.x
+//  def right = bottomRight.x
+//  def width = right - left
+//  // and many more geometric methods...
+//}
+
+trait Rectanglular {
   def topLeft: Point
   def bottomRight: Point
 
   def left = topLeft.x
   def right = bottomRight.x
   def width = right - left
+  // and many more geometric methods...
 }
+
+abstract class Component extends Rectanglular {
+  // other methods...
+}
+
 
 
