5f0fa180c3dd3d79890849d70999f844e149c70c
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2019-06-24 12:06:47 +0800

    Property Checks

---
+++ ch14-assertionsAndTests/ElemScalaCheck.scala
@@ -0,0 +1,17 @@
+import org.scalatest.WordSpec
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.MustMatchers._
+import Element.elem
+
+class ElemScalaCheck extends WordSpec with PropertyChecks {
+
+  "elem result" must {
+    "have passed width" in {
+      forAll { (w: Int) =>
+        whenever (w > 0) {
+          elem('x', w, 3).width must equal (w)
+        }
+      }
+    }
+  }
+}
