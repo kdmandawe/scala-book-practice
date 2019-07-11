83b7ac5521e95aa843a93968038cc51b0676a990
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2019-07-11 10:54:04 +0800

    Option

---
+++ ch15-caseClassesAndPatternMatching/OptionType.scala
@@ -0,0 +1,17 @@
+object OptionType extends App {
+
+  val capitals = Map("France" -> "Paris", "Japan" -> "Tokyo")
+
+  println(capitals get "France")
+  println(capitals get "North Pole")
+
+  def show(x: Option[String]) = x match {
+    case Some(s) => s
+    case None => "?"
+  }
+
+  println(show(capitals get "Japan"))
+  println(show(capitals get "France"))
+  println(show(capitals get "North Pole"))
+
+}
